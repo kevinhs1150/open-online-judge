c10a21eac4bc0d4f6c9e4d42d606d83c4e2e523a
fishy0903 <fishy0903@e3896563-57da-0129-da3f-310c26432eee>
2011-04-10 09:21:05 +0000

    Client GUI 第一版 各物件的名稱還沒定，所以有很多類似Button1這種不太好的命名，之後再做修改。

---
+++ trunk/team/include/ClientMain.h
@@ -0,0 +1,19 @@
+#ifndef CLIENTMAIN_H_
+#define CLIENTMAIN_H_
+
+#include <wx/wx.h>
+#include "ClientApp.h"
+#include "ClientGUI.h"
+
+class ClientFrame: public ClientGUI
+{
+    public:
+        ClientFrame(wxFrame *frame);
+        ~ClientFrame();
+    private:
+        void OnClose(wxCloseEvent& event);
+        DECLARE_EVENT_TABLE()
+};
+
+
+#endif // CLIENTMAIN_H_
